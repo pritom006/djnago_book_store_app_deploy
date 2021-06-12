4936b95bb69702c729f0abd2b77eea03133e1e8a
pritom006 <banerjeecse08@gmail.com>
2021-06-12 20:05:17 +0600

    allow host added

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
-ALLOWED_HOSTS = ['fierce-shelf-96270.herokuapp.com']
+ALLOWED_HOSTS = ['aqueous-earth-54453.herokuapp.com']
 
 
 # Application definition
